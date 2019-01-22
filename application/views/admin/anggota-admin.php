bac10e50c0c8ebb6409d13dca834bd1434dfb8db
Nur Amin Nudin <nur.aminnudin@gmail.com>
2019-01-22 15:23:48 +0700

    Merapikan2

--- application/views/admin/anggota-admin.php
+++ application/views/admin/anggota-admin.php
@@ -12,10 +12,10 @@
             </button>
           </div>
           <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
+            <!-- <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Data Anggota</li>
-            </ol>
+            </ol> -->
           </div>
         </div>
       </div><!-- /.container-fluid -->
--- application/views/admin/pinjam-admin.php
+++ application/views/admin/pinjam-admin.php
@@ -7,10 +7,10 @@
             <h1>Data Peminjaman Barang</h1>
           </div>
           <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
+            <!-- <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Data Peminjaman Barang</li>
-            </ol>
+            </ol> -->
           </div>
         </div>
       </div><!-- /.container-fluid -->
