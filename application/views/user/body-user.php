82788769909d491bb850e0800aacf927bc029e57
Nur Amin Nudin <nur.aminnudin@gmail.com>
2019-02-01 15:03:52 +0700

    ganti user

--- application/views/user/body-user.php
+++ application/views/user/body-user.php
@@ -1,103 +1,19 @@
-<!-- First Container -->
-<div class="container-fluid bg-2 text-center">
-    <h3 class="margin">Pencarian</h3>
-        <form class="form-inline">
-            <div class="input-group">
-                <input type="email" class="form-control" size="50" placeholder="Cari . . . . ." required>
-                <div class="input-group-btn">
-                    <button type="button" class="btn btn-danger">Cari</button>
-                </div>
+<div class="awak">
+        <div class="content">
+          <div class="row">
+            <div class="col-md-8">
+              <div class="bodikiri"></div>
             </div>
-        </form>
-    <h3></h3>
-</div>
-
-<!-- Second Container -->
-<div class="container-fluid bg-1 text-center">
-    <div class="col-md-5">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Data Barang</h3>
+            <div class="col-md-4">
+              <div class="bodikanan"></div>
             </div>
-            
-            <!-- /.card-header -->
-            <div class="card-body">
-                <table id="example1" class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Kode barang</th>
-                            <th>Nama Barang</th>
-                            <th>Tersedia</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i=1; foreach($tabel_record as $row){ ?>
-                            <tr>
-                                <td><?php echo $i; ?></td>
-                                <td><?php echo $row->kode_barang ?></td>
-                                <td><?php echo $row->nama_barang ?></td>
-                                <td><?php echo $row->jml_tersedia ?></td>
-                                <?php $i++; ?>
-                            </tr>
-
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
+          </div>
         </div>
-    </div>
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-5">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Data Peminjaman</h3>
-              
-            </div>
-            
-            <!-- /.card-header -->
-            <div class="card-body">
-                <table id="example1" class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Peminjam</th>
-                            <th>Nama Barang</th>
-                            <th>Jumlah Barang</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i=1; foreach($tabel_record as $row){ ?>
-                            <tr>
-                                <td><?php echo $i; ?></td>
-                                <td><?php echo $row->nama_barang ?></td>
-                                <td><?php echo $row->nama_barang ?></td>
-                                <td><?php echo $row->jml_tersedia ?></td>
-                                <?php $i++; ?>
-                            </tr>
 
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Third Container (Grid) -->
-<div class="container-fluid bg-3 text-center">    
-    <h3 class="margin">Where To Find Me?</h3><br>
-        <div class="row">
-            <div class="col-sm-4">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <div class="col-sm-4"> 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-            <div class="col-sm-4"> 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-    </div>
-</div>
+      </div>
+
+      
+
+      
+
+    </main>
--- application/views/user/footer-user.php
+++ application/views/user/footer-user.php
@@ -1,68 +1,29 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2019 <a href="http://adminlte.io">Power By AdminLTE</a>.</strong>
-    Team Udinus
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.0
-    </div>
-  </footer>
+<footer class="container">
+      <p>&copy; team dinus</p>
+    </footer>
 
-</div>
-<!-- ./wrapper -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="<?php echo base_url()?>user/assets/js/vendor/popper.min.js"></script>
+    <script src="<?php echo base_url()?>user/dist/js/bootstrap.min.js"></script>
+    <script>
+      window.onscroll = function() {myFunction()};
 
-<!-- jQuery -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/datatables/jquery.dataTables.js"></script>
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/datatables/dataTables.bootstrap4.js"></script>
-<!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/morris/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="<?php echo base_url() ?>admin-lte-master/plugins/fastclick/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="<?php echo base_url() ?>admin-lte-master/dist/js/adminlte.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url() ?>admin-lte-master/dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url() ?>admin-lte-master/dist/js/demo.js"></script>
-<script>
-  $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false
-    });
-  });
-</script>
+      var navbar = document.getElementById("navbar");
+      var sticky = navbar.offsetTop;
+
+      function myFunction() {
+        if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky")
+        } else {
+          navbar.classList.remove("sticky");
+        }
+      }
+    </script>
 
 
-
-</body>
+  </body>
 </html>
