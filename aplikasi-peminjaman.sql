d52a22f29cc451e1719cfdf88fbe28cc0e036e35
Nur Amin Nudin <nur.aminnudin@gmail.com>
2019-03-14 20:57:11 +0700

    Update db terbaru

--- aplikasi-peminjaman.sql
+++ aplikasi-peminjaman.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 18 Feb 2019 pada 03.20
+-- Generation Time: 14 Mar 2019 pada 14.55
 -- Versi Server: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -33,8 +33,6 @@ CREATE TABLE `aktifitas_pinjam` (
   `kd_pjm` varchar(5) NOT NULL,
   `nip` varchar(20) NOT NULL,
   `kd_brg` varchar(5) NOT NULL,
-  `jml_pjm` int(4) NOT NULL,
-  `jml_kmbl` int(4) NOT NULL,
   `tgl_pjm` date NOT NULL,
   `estimate_kmbl` date NOT NULL,
   `tgl_kmbl` date NOT NULL,
@@ -47,10 +45,12 @@ CREATE TABLE `aktifitas_pinjam` (
 -- Dumping data untuk tabel `aktifitas_pinjam`
 --
 
-INSERT INTO `aktifitas_pinjam` (`id`, `kd_pjm`, `nip`, `kd_brg`, `jml_pjm`, `jml_kmbl`, `tgl_pjm`, `estimate_kmbl`, `tgl_kmbl`, `ptgs_pjm`, `ptg_kmbl`, `status`) VALUES
-(31, '7063', '33667', '2812', 1, 0, '2019-02-17', '2019-02-18', '0000-00-00', '11001', '-', 'Belum Kembali'),
-(32, '7063', '33667', '5097', 1, 0, '2019-02-17', '2019-02-18', '0000-00-00', '11001', '-', 'Belum Kembali'),
-(33, '7063', '33667', '7302', 0, 1, '2019-02-17', '2019-02-18', '2019-02-17', '11001', '11001', 'Telah Kembali');
+INSERT INTO `aktifitas_pinjam` (`id`, `kd_pjm`, `nip`, `kd_brg`, `tgl_pjm`, `estimate_kmbl`, `tgl_kmbl`, `ptgs_pjm`, `ptg_kmbl`, `status`) VALUES
+(65, '7569', '88776', '3252', '2019-03-13', '2019-03-20', '0000-00-00', 'admin', '-', 'Belum Kembali'),
+(66, '7569', '88776', '4046', '2019-03-13', '2019-03-20', '0000-00-00', 'admin', '-', 'Belum Kembali'),
+(67, '7569', '88776', '6107', '2019-03-13', '2019-03-20', '2019-03-14', 'admin', '', 'Kembali'),
+(68, '7569', '88776', '5301', '2019-03-13', '2019-03-20', '0000-00-00', 'admin', '-', 'Belum Kembali'),
+(69, '7569', '88776', '1130', '2019-03-13', '2019-03-20', '0000-00-00', 'admin', '-', 'Belum Kembali');
 
 -- --------------------------------------------------------
 
@@ -74,7 +74,6 @@ CREATE TABLE `akun_admin` (
 INSERT INTO `akun_admin` (`id`, `username`, `level_user`, `password`, `token`, `status`) VALUES
 (1, 'admin', 'admin', '827ccb0eea8a706c4c34a16891f84e7b', '21232f297a57a5a743894a0e4a801fc3', 1),
 (2, '11001', 'petugas', '827ccb0eea8a706c4c34a16891f84e7b', '64254db8396e404d9223914a0bd355d2', 1),
-(3, '99005', 'petugas', 'b0baee9d279d34fa1dfd71aadb908c3f', 'be548e4593c60a91f0b9efcd28742167', 1),
 (5, '66672', 'admin', 'b0baee9d279d34fa1dfd71aadb908c3f', 'f9454bdf67218089cace948e7da7d88e', 1),
 (7, '339999', 'petugas', 'b0baee9d279d34fa1dfd71aadb908c3f', 'bb7b09568bf8641dfb1fcac2992f50c7', 1);
 
@@ -108,7 +107,7 @@ INSERT INTO `anggota` (`id`, `nip`, `nama`, `jabatan`, `pangkat_golongan`, `seks
 (9, '99887', 'Nisya Purnamasari', 'EBT', 'Non', 'Sekretariat', '1994-12-09', ''),
 (10, '55442', 'Tugiman Slamet', 'Program', '4/b', 'Sekretariat', '1980-12-20', ''),
 (11, '99655', 'Handayani S', 'Kelistrikan', '3/a', 'Sekretariat', '1995-09-09', ''),
-(12, '66672', 'Marni Styani', 'Keuangan', '4/d', 'Sekretariat', '1992-09-17', ''),
+(12, '66672', 'Marni Styani', 'Keuangan', '4/d', 'Sekretariat', '1992-09-17', 'drow1.jpg'),
 (13, '443321', 'Mahmud Dwi Yanto', 'EBT', '4/c', 'Sekretariat', '1983-11-08', ''),
 (14, '334421', 'Maryanto S', 'Keuangan', '4/a', 'Sekretariat', '1991-05-19', 'A11201610010.jpg'),
 (15, '339999', 'Dody Setiawan', 'Kepala EBT', '4/a', 'Sekretariat', '1998-12-12', 'A11_2016_099361.jpg');
@@ -126,10 +125,8 @@ CREATE TABLE `barang` (
   `merk` varchar(30) NOT NULL,
   `kategori` varchar(20) NOT NULL,
   `tgl_masuk` date NOT NULL,
-  `jml_terpinjam` int(4) NOT NULL,
   `spesifikasi` varchar(50) NOT NULL,
-  `jml_barang` int(4) NOT NULL,
-  `jml_tersedia` int(4) NOT NULL,
+  `status` int(1) NOT NULL,
   `foto` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -137,23 +134,15 @@ CREATE TABLE `barang` (
 -- Dumping data untuk tabel `barang`
 --
 
-INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `merk`, `kategori`, `tgl_masuk`, `jml_terpinjam`, `spesifikasi`, `jml_barang`, `jml_tersedia`, `foto`) VALUES
-(9, '9668', 'Mobil', 'Honda Brio', 'Kendaraan', '2019-02-07', 0, 'Tahun 2017', 3, 3, ''),
-(10, '6587', 'Motor Matic', 'Honda Beat', 'Kendaraan', '2019-02-07', 0, '110 cc Tahun 2016', 4, 4, 'Honda-Beat1.jpeg'),
-(11, '4345', 'Kamera Dron', 'Canon', 'Elektonik', '2019-02-06', 0, 'Lensa 12 mm', 2, 2, ''),
-(12, '4981', 'Printer', 'Epson', 'Elektonik', '2019-02-07', 0, 'E300', 10, 10, ''),
-(13, '2396', 'Monitor', 'BenQ', 'Elektonik', '2019-02-05', 0, '16 inci', 4, 4, ''),
-(14, '7680', 'Kursi', '-', 'Prabotan', '2019-02-07', 0, 'Kursi audience', 25, 25, ''),
-(15, '6132', 'Meteran', '-', 'Lain-lain', '2019-02-01', 0, 'Ukuran 500 m', 20, 20, ''),
-(16, '7015', 'Tang Ampere', 'Simbada', 'Lain-lain', '2019-02-07', 0, 'Max 10 ampere', 7, 7, ''),
-(17, '7566', 'Distance Meter', '-', 'Lain-lain', '2019-02-07', 0, '5 Meter', 5, 5, ''),
-(18, '2812', 'Gas Detector', 'Hitaci', 'Lain-lain', '2019-02-01', 1, 'Detect 100 m', 3, 2, ''),
-(19, '7302', 'Helm', 'INK', 'Lain-lain', '2019-02-04', 1, 'SNI', 10, 9, ''),
-(20, '2139', 'Kompas', '-', 'Lain-lain', '2019-01-29', 0, 'Kompas 4 inci', 10, 10, ''),
-(21, '5097', 'GPS', 'Nokia', 'Lain-lain', '2019-02-05', 1, '5 inci', 4, 3, ''),
-(22, '4691', 'Motor', 'Honda SupraX', 'Kendaraan', '2019-02-08', 0, '125 cc', 2, 2, 'sampleMotor.jpg'),
-(23, '9550', 'Motor', 'Yamaha Mio', 'Kendaraan', '0000-00-00', 0, 'Tahun 2016', 3, 3, '6903-woman-darkness.jpg'),
-(24, '7569', 'Lemari', 'Olimpic', 'Lain-lain', '2019-02-18', 0, '2x4 meter', 4, 4, 'logo_esdm.png');
+INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `merk`, `kategori`, `tgl_masuk`, `spesifikasi`, `status`, `foto`) VALUES
+(29, '8492', 'Kompas', 'gigabit', 'teknis', '2019-02-21', 'anti magnet', 1, 'logo_1.jpg'),
+(30, '1130', 'Buku Kimia', 'Gmedia', 'perpus', '2019-02-21', '300 halaman', 0, 'texture-red-brick-wall.jpg'),
+(31, '2271', 'Gelas', 'BBC', 'lain-lain', '2019-02-26', 'kaca', 1, 'warna1.jpg'),
+(32, '5136', 'Flasdisk', 'Sandisk', 'elektronik', '2019-03-10', '4 GB', 1, 'pilihan.png'),
+(33, '6107', 'asfa', 'asfasdf', 'lain-lain', '2019-03-06', 'afasfsdf', 0, 'img_bg_1.jpg'),
+(34, '3252', 'asd', 'sdasdadas', 'teknis', '0000-00-00', 'asdfsdfadsfasdfsd', 0, 'Screenshot_(3).png'),
+(35, '5301', 'asfaasf', 'asdfsfs', 'lain-lain', '0000-00-00', 'asfasdf asdf', 0, 'Screenshot_(12).png'),
+(36, '4046', 'asdvzxcvzcxvas', 'asdfvszdv v', 'perpus', '2019-03-07', 'sfdsadf', 0, 'Screenshot_(8).png');
 
 -- --------------------------------------------------------
 
@@ -176,7 +165,9 @@ CREATE TABLE `kembali_brg` (
 --
 
 INSERT INTO `kembali_brg` (`id`, `nip`, `kode_brg`, `tgl_pinjam`, `estimasi`, `wkt_kembali`, `petugas_kmbl`) VALUES
-(43, '33667', '7302', '2019-02-17', '2019-02-18', '2019-02-17', '11001');
+(52, '33667', '8492', '2019-02-21', '2019-02-28', '2019-02-21', 'admin'),
+(53, '99887', '8492', '2019-02-21', '2019-02-28', '2019-02-21', 'admin'),
+(54, '33667', '8492', '2019-02-26', '2019-03-05', '2019-02-26', 'admin');
 
 -- --------------------------------------------------------
 
@@ -205,8 +196,9 @@ CREATE TABLE `pinjam_barang` (
 --
 
 INSERT INTO `pinjam_barang` (`id`, `kd_pinjam`, `nip`, `nama`, `jabatan`, `seksi`, `kode_barang`, `nama_barang`, `jml_pinjam`, `tgl_pinjam`, `tgl_kembali`, `petugas`, `status`) VALUES
-(54, '7063', '33667', 'Sudirman Wahab', 'Kepegawean/Umum', 'Sekretariat', '2812', 'Gas Detector', 1, '2019-02-17', '2019-02-18', '11001', 'Belum Kembali'),
-(55, '7063', '33667', 'Sudirman Wahab', 'Kepegawean/Umum', 'Sekretariat', '5097', 'GPS', 1, '2019-02-17', '2019-02-18', '11001', 'Belum Kembali');
+(18, '7569', '88776', 'Manto Widodo', 'Program', 'Sekretariat', '3252', 'asd', 1, '2019-03-13', '2019-03-20', 'admin', 'Belum Kembali'),
+(21, '7569', '88776', 'Manto Widodo', 'Program', 'Sekretariat', '5301', 'asfaasf', 1, '2019-03-13', '2019-03-20', 'admin', 'Belum Kembali'),
+(22, '7569', '88776', 'Manto Widodo', 'Program', 'Sekretariat', '1130', 'Buku Kimia', 1, '2019-03-13', '2019-03-20', 'admin', 'Belum Kembali');
 
 --
 -- Indexes for dumped tables
@@ -258,7 +250,7 @@ ALTER TABLE `pinjam_barang`
 -- AUTO_INCREMENT for table `aktifitas_pinjam`
 --
 ALTER TABLE `aktifitas_pinjam`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `akun_admin`
@@ -276,19 +268,19 @@ ALTER TABLE `anggota`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `kembali_brg`
 --
 ALTER TABLE `kembali_brg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `pinjam_barang`
 --
 ALTER TABLE `pinjam_barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
